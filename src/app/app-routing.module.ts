6bef292847cdd3ab7cee87bbf1b8b804e7af9428
BurbaCodes <dilushadasanayaka@gmail.com>
2019-07-09 03:06:00 +0530

    add post page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
+
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,4 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+
+  
+  title;
+  price;
+  description;
+  model;
+  
+  constructor(){}
+
+  processForm(event) {
+    //event.preventDefault();
+    console.log(this.title,this.description);
+  }
+  
+ handleTitle(event) {
+    this.title = event.target.value;
+    console.log(this.title,this.description);
+  }
+
+  handlePrice(event) {
+    this.price = event.target.value;
+  }
+
+  handleModel(event) {
+    this.model = event.target.value;
+  }
+
+  handleDes(event) {
+    this.description = event.target.value;
+  }
+}
